c56e04844256e45c9556a5bf5e859d04bdcc07f1
Ana Madalena <64982998+RedSquirrel98@users.noreply.github.com>
2020-06-16 20:55:32 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,21 +1,24 @@
 import socket
-import select
+import selectors
 
-IP = '127.0.0.1
+IP = '127.0.0.1'
 PORT= 5550
-
 HEADER_LENGTH= 10
 server_socket= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1) # allows us to reuse port number upon reconnection
 server_socket.bind((IP, PORT))
 server_socket.listen()  # socket listens for incoming connections, will become available to read from when a connection is pending
-
- 
-sockets_list=[server_socket] # create a blank list of active sockets
-clients= {}
+server_socket.setblocking(False)
+message=[]
 
 print(f'Listening for connections on {IP}:{PORT}...')
+ 
+clients= {}     # create a dictionary where user socket object ids and usernames are stored. Any new user that signs up will be
+                # prompted to create a username, which is then stored as { fileobj : username} 
+client_messages={}                
+select=selectors.DefaultSelector()
+select.register(server_socket, selectors.EVENT_READ, data= None)
+
 
 def receive_message(client_socket):
     try:
@@ -27,65 +30,54 @@ def receive_message(client_socket):
     except:
         return False    
 
-while True:
-    
 
-    # The first is a list of the objects to be checked for incoming data to be read, the second contains objects
-    #  that will receive outgoing data when there is room in their buffer, and the third list of those that may have an error 
-    read_sockets,_,exception_sockets= select.select(sockets_list,[],sockets_list)  # SELECT method interfaces with the operating system functions to poll a list of available sockets
-     
-    for notified_socket in read_sockets:
-                 
-         # A "readable" server socket is ready to accept a connection
-        if notified_socket == server_socket:  
-            client_conn, client_addr= server_socket.accept()  # conn is a new socket object usable to send and receive data, addr is the address bound to the socket on the other end of the connection.
-            user = receive_message(client_conn)                                                               
-            
-            # !!! this is a very poor way to receive messages: instead, check if client sockets have become readable and place them into a queue. 
-            # here we are assuming that while connecting the client will send its username
-            if user is False:  #this means user has disconnected before sending its data
-            
-                pass 
-            sockets_list.append(client_conn) # add the client socket object to the current list of active sockets      
-            clients[client_conn] = user # relate dictionary message data to client in array   
-            print('Accepted new connection from {}:{}, username: {}'.format(*client_addr, user['data'].decode('utf-8'))) 
+def new_connection(server_sockobj):
+
+    client_conn, client_addr= server_sockobj.accept()  # conn is a new socket object usable to send and receive data, addr is the address bound to the socket on the other end of the connection.
+    client_conn.setblocking(False)                     # set new socket object to be non-blocking
+    username = receive_message(client_conn)            # when connecting for the first time to the socket, we will received the username only
+    
+    if username is False:
+        print('New user has disconnected')  #this means user has disconnected before sending its data 
+ 
+    print('Accepted new connection from {}:{}, username: {}'.format(*client_addr, username['data'].decode('utf-8')))
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+                          # save username and blank message for later
+    select.register(client_conn, events, data=username)  # register the new socket object and username for monitoring
+    # Find a way to handle for KeyError here, if the file object is already registered.
+    clients[client_conn] = username # relate dictionary message data to client in array  
+    return clients                                     
+
+def messaging_function(key,events,clients):
+    client_conn= key.fileobj
+    username= key.data
+    if events & selectors.EVENT_READ:
+        message= receive_message(client_conn) # receive the message from the user
         
-        # !!! again, this is a very poor way to receive messages: instead, check if client sockets have become readable and place them into a queue. 
-        # if one of the already established socket client connections is readable, this means it is ready to send data
-        else:
+        if message is False:
+            print('Closed connection from: {}'.format(username['data'].decode('utf-8')))
+            select.unregister(client_conn)
+            del clients[client_conn]
+            client_conn.close()
+            return()
 
-            message= receive_message(notified_socket) # pass the message into a reading queue later on
-            if message is False:
-                 
-                print('Closed connection from: {}'.format(clients[notified_socket]['data'].decode('utf-8')))
-                 
-                sockets_list.remove(notified_socket)
-                 
-                del clients[notified_socket] #remove the client socket from the dictionary if it has disconnected
-                 
-                continue
+        namedisplay=username['data'].decode("utf-8")
+        messagedisplay=message['data'].decode("utf-8")        
+        print(f'Received message from {namedisplay}: {messagedisplay}')
+        for sending_socks in clients:
+            if sending_socks != client_conn: 
+                sending_socks.send(username['header'] + username['data'] + message['header'] + message['data'])
 
-            user = clients[notified_socket]
-            namedisplay=user['data'].decode("utf-8")
-            messagedisplay=message['data'].decode("utf-8")
-            print(f'Received message from {namedisplay}: {messagedisplay}')
-           
+while True: 
+    sockets_tuple= select.select(timeout=None)   # this call will return a dict  of all socket
+                                                 # objects registered as well as a mask indicating whether they are read or write ready.
 
-
-            for client_conn in clients:
-                if client_conn != notified_socket: 
-                    
-                        
-                    client_conn.send(user['header'] + user['data'] + message['header'] + message['data'])
     
-    for notified_socket in exception_sockets:
-
-        # Remove from list for socket.socket()
-        sockets_list.remove(notified_socket)
-
-         # Remove from our list of users
-        del clients[notified_socket]                 
-
+    for key, events in sockets_tuple:            # accessing each part of the dict
+        if key.data is None:                     # if key has no data (username) then we have an unregistered user waiting on a server connection
+            clients= new_connection(key.fileobj)
+        else:
+            messaging_function(key, events,clients)      # if username is present then we need to service the read or write request
+ 
 
 
-                
